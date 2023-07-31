43f528d2165c4db7d519d80fca3eb17530ae2be8
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2023-07-31 22:40:44 +0300

    Lol, organize the parser state machine

--- json_types.go
+++ json_types.go
@@ -1,5 +1,45 @@
 package gojason
 
-type JsonNumberType struct {
+type JsonValueNumber struct {
 	payload []rune
+}
+
+func (jsonValueNumber *JsonValueNumber) asAny() JsonValueAny {
+	return JsonValueAny{
+		number: jsonValueNumber,
+	}
+}
+
+func newJsonValueNumber(payload []rune) JsonValueNumber {
+	return JsonValueNumber{
+		payload: payload,
+	}
+}
+
+type JsonValueObject struct {
+	values map[JsonValueObjectKey]JsonValueAny
+}
+
+func (jsonValueObject *JsonValueObject) asAny() JsonValueAny {
+	return JsonValueAny{
+		object: jsonValueObject,
+	}
+}
+
+func newJsonValueObject(keyValues map[JsonValueObjectKey]JsonValueAny) JsonValueObject {
+	return JsonValueObject{
+		values: keyValues,
+	}
+}
+
+type JsonValueAny struct {
+	number *JsonValueNumber
+	object *JsonValueObject
+}
+
+type JsonValueObjectKey = JsonValueAny
+type JsonValueObjectKeyValues = map[JsonValueObjectKey]JsonValueAny
+
+func newJsonValueObjectKeyValues() JsonValueObjectKeyValues {
+	return make(JsonValueObjectKeyValues)
 }
--- parser.go
+++ parser.go
@@ -1,38 +1,20 @@
 package gojason
 
-type jsonContextType int
-
-const (
-	JCTObjectOpened jsonContextType = iota
-)
-
-type jsonContext struct {
-	jsonContextType jsonContextType
-}
-
-func newJsonContext(jsonContextType jsonContextType) jsonContext {
-
-}
-
-func parse(jsonString string) error {
+func parse(jsonString string) (*JsonValueAny, error) {
 	tokenSearch := newTokenSearch(jsonString)
 
 	var firstTokenSearchResult = tokenSearch.findNonWhitespaceToken()
 
 	var err = firstTokenSearchResult.err
 	if err != nil {
-		return w(err, "token search failed")
+		return nil, w(err, "token search failed")
 	}
 
 	var firstToken = firstTokenSearchResult.token
 
 	if firstToken == nil {
-		return e("haven't found any token in this jsonString")
+		return nil, e("haven't found any token in this jsonString")
 	}
-
-	var context []jsonContext
-
-	var 
 
 	switch firstToken.tokenType {
 	case invalidoTokenType:
@@ -42,27 +24,118 @@ func parse(jsonString string) error {
 	case jsonWhitespaceTokenType:
 		panic("I called findNonWhitespaceToken ;)")
 	case jsonColonTokenType:
-		return e("colon can not be the first token in a json")
+		return nil, e("colon can not be the first token in a json")
 	case jsonCurlyOpenBracketTokenType:
 		// now we're talking
-		context = []jsonContext{jsonContext{jsonContextType: JCTObjectOpened}}
+		var parsedObject, err = parseJsonObjectAfterItJustStarted(&tokenSearch)
+
+		if err != nil {
+			return nil, e("parsing root object failed")
+		}
+
+		var any = parsedObject.asAny()
+
+		return &any, nil
 	case jsonCurlyClosingBracketTokenType:
-		return e("curly closing bracket can not be the first token in a json")
+		return nil, e("curly closing bracket can not be the first token in a json")
 	case jsonCommaTokenType:
-		return e("comma can not be the first token in a json")
+		return nil, e("comma can not be the first token in a json")
 	default:
 		panic("unhandled token type")
 	}
+}
 
+type parseJsonObjectState int
+
+const (
+	PJOWaitingForKey parseJsonObjectState = iota
+	PJOGotKeyWaitingForSeparator
+)
+
+func parseJsonObjectAfterItJustStarted(tokenSearch *tokenSearch) (*JsonValueObject, error) {
 	for {
-		tokenSearchResult := tokenSearch.findNonWhitespaceToken()
+		var tokenSearchResult = tokenSearch.findNonWhitespaceToken()
 
-		if tokenSearchResult.err != nil {
-			return w(err, "token search failed")
+		var err = tokenSearchResult.err
+		if err != nil {
+			return nil, w(err, "token search failed")
 		}
 
-		if tokenSearchResult.token == nil {
-			// this means json ended
+		var state = PJOWaitingForKey
+		var keyValuePairs = newJsonValueObjectKeyValues()
+		var parsedKey *JsonValueAny
+
+		var token = tokenSearchResult.token
+		if token == nil {
+			switch state {
+			case PJOWaitingForKey:
+				return nil, e("unexpected lack of token while waiting for first field in the object")
+			case PJOGotKeyWaitingForSeparator:
+				return nil, e("unexpected lack of token while waiting for separator after key")
+			default:
+				panic("unhandle parse json object state")
+			}
+		}
+
+		switch token.tokenType {
+		case invalidoTokenType:
+			panic("noo")
+		case jsonNumberTokenType:
+			switch state {
+			case PJOWaitingForKey:
+				var numberValue = newJsonValueNumber(token.payload)
+				var anyValue = numberValue.asAny()
+				parsedKey = &anyValue
+				state = PJOGotKeyWaitingForSeparator
+			case PJOGotKeyWaitingForSeparator:
+				return nil, e("expected colon, but there was suddenly a number after a key")
+			default:
+				panic("unhandled parse obj state")
+			}
+		case jsonWhitespaceTokenType:
+			panic("I said, first non-whitespace, please!")
+		case jsonColonTokenType:
+			switch state {
+			case PJOWaitingForKey:
+				return nil, e("expected a start of a json object, got colon instead")
+			case PJOGotKeyWaitingForSeparator:
+				// das gud
+
+			default:
+				panic("unhandled parse obj state")
+			}
+		case jsonCurlyOpenBracketTokenType:
+			switch state {
+			case PJOWaitingForKey:
+				return nil, e("expected a start of a json object, got curly open bracket. What are you, trying to make an object inside an object?")
+			case PJOGotKeyWaitingForSeparator:
+				return nil, e("expected colon, but there was suddenly a new object start")
+			default:
+				panic("unhandled parse obj state")
+			}
+		case jsonCurlyClosingBracketTokenType:
+			switch state {
+			case PJOWaitingForKey:
+				// welp
+				var jsonObject = newJsonValueObject(keyValuePairs)
+
+				return &jsonObject, nil
+			case PJOGotKeyWaitingForSeparator:
+				return nil, e("expected colon, but there was suddenly an end of the current object")
+			default:
+				panic("unhandled parse obj state")
+			}
+		case jsonCommaTokenType:
+			switch state {
+			case PJOWaitingForKey:
+				return nil, e("expected a start of a json object, got a comma")
+			case PJOGotKeyWaitingForSeparator:
+				return nil, e("expected colon, but there was suddenly a comma")
+			default:
+				panic("unhandled parse obj state")
+			}
+		default:
+			panic("token type unhandled")
 		}
 	}
 }
